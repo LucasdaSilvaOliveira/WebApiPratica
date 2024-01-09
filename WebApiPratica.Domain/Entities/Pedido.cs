27aea06156a2103752c36a456de689d0662e64e4
Lucas Silva <lucas-silva-98@hotmail.com>
2024-01-09 10:08:39 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiPratica.Domain/Entities/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiPratica.Domain.Entities
+{
+    public class Pedido
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Lanche { get; set; }
+
+        public int ClienteId { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public Cliente Cliente { get; set;}
+    }
+}
