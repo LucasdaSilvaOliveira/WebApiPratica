11816c4455478f1a80f8ecbe99684bdd40a87f1a
Lucas Silva <lucas-silva-98@hotmail.com>
2024-01-10 21:53:33 -0300

    clear: Limpando código

--- WebApiPratica.Infra/Repositories/PedidoRepository/PedidoRepository.cs
+++ WebApiPratica.Infra/Repositories/PedidoRepository/PedidoRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebApiPratica.Domain.Entities;
+﻿using WebApiPratica.Domain.Entities;
 using WebApiPratica.Infra.Data;
 
 namespace WebApiPratica.Infra.Repositories.PedidoRepository
