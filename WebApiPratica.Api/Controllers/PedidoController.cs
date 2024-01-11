92e9781e8cfa951bab4eb70bb9d9f6fcd3bf0b4c
Lucas Silva <lucas-silva-98@hotmail.com>
2024-01-10 22:06:39 -0300

    feat: Implementação do PedidoController e rota de ObterTodos e ObterPorId

---
+++ WebApiPratica.Api/Controllers/PedidoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiPratica.Infra.Repositories.PedidoRepository;
+
+namespace WebApiPratica.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoController : ControllerBase
+    {
+        private IPedidoRepository _pedidoRepository;
+        public PedidoController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
+        [HttpGet]
+        [Route("ObterTodos")]
+        public IActionResult ObterTodos()
+        {
+            try
+            {
+                var pedidos = _pedidoRepository.ObterTodos();
+                return Ok(pedidos);
+            } catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("ObterPorId")]
+        public IActionResult ObterPorId(int id)
+        {
+            try
+            {
+                var pedido = _pedidoRepository.ObterPorId(id);
+                return Ok(pedido);
+            } catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
